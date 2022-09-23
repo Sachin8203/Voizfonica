5e68c2e6b7c026007b977beeda279d09ac923ad1
Sachin8203 <108717914+Sachin8203@users.noreply.github.com>
2022-09-23 18:42:44 +0530

    Delete demoproject directory

--- demoproject/src/main/java/com/demoproject/demoproject/Repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demoproject.demoproject.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demoproject.demoproject.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer>{
-
-}
