7217b1b5e451bf744fae86721771755a96f9a41b
Sachidhanantha D <sachidhanantha.d@prodapt.com>
2022-09-23 16:45:02 +0530

    first commit

---
+++ src/app/postpaidsimcard/postpaidsimcard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postpaidsimcard',
+  templateUrl: './postpaidsimcard.component.html',
+  styleUrls: ['./postpaidsimcard.component.css']
+})
+export class PostpaidsimcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  sim(){
+    {alert("Details submited successfully");}
+  }
+
+}
